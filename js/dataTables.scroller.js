c4da093277341f660241fc9cc5d733e27acf7d65
SandyDatatables <sandy@datatables.net>
2022-02-10 16:51:05 +0000

    fix: Make scroller retain state save info
    Jira Issue DD-2431

--- js/dataTables.scroller.js
+++ js/dataTables.scroller.js
@@ -533,6 +533,7 @@ $.extend( Scroller.prototype, {
 			if ( initialStateSave && loadedState ) {
 				data.scroller = loadedState.scroller;
 				initialStateSave = false;
+				that.s.lastScrollTop = data.scroller.scrollTop;
 			}
 			else {
 				// Need to used the saved position on init
