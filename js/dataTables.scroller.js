08d28da6c195b5d4301e63e9978b28486892eb45
Allan Jardine <allan@datatables.net>
2023-07-04 14:47:42 +0100

    Release 2.2.0

--- js/dataTables.scroller.js
+++ js/dataTables.scroller.js
@@ -1,11 +1,11 @@
-/*! Scroller 2.2.0-dev
+/*! Scroller 2.2.0
  * © SpryMedia Ltd - datatables.net/license
  */
 
 /**
  * @summary     Scroller
  * @description Virtual rendering for DataTables
- * @version     2.2.0-dev
+ * @version     2.2.0
  * @author      SpryMedia Ltd (www.sprymedia.co.uk)
  * @contact     www.sprymedia.co.uk/contact
  * @copyright   SpryMedia Ltd.
@@ -1173,7 +1173,7 @@ Scroller.oDefaults = Scroller.defaults;
  *  @name      Scroller.version
  *  @static
  */
-Scroller.version = '2.2.0-dev';
+Scroller.version = '2.2.0';
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  * Initialisation
